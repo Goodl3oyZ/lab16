bd32f393a435daa6424f23653aba0ed8829f1cdc
Goodl3oyZ <panudetzx2@gmail.com>
2023-02-07 15:18:12 +0700

    16.1 complete string not char

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,22 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
-	
-	
+	int a=5;
+    string b="A";
+    string &c=b;
+	int *x=&a;
+	string *y=&b;
+	int **z=&x;
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
+	cout<<&a<<" "<<&b<<" "<<&c<<" "<<&x<<" "<<&y<<" "<<&z<<endl;
+	c="F";
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
+	*y='W';
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
+	*x=6;
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
+	**z=7;
+	cout<<a<<" "<<b<<" "<<c<<" "<<x<<" "<<y<<" "<<z<<endl;
 	return 0;
 }
